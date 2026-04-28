bbcf89375427bbf71c25a2d3180011da09c4e56c
David Cruz <david@codebycruz.com>
2026-04-28 01:02:40 -0700

    refactor!: replace libgit2 submodule with lde-build usage
    This simplifies the build script with the introduction of lde-build and
    allows for cloning the repo without any need for recursing submodules.

--- build.lua
+++ build.lua
@@ -1,46 +1,43 @@
-local outDir = os.getenv("LDE_OUTPUT_DIR")
-local sep = string.sub(package.config, 1, 1)
+local build = require("lde-build")
+
 local isWindows = jit.os == "Windows"
 local isMac = jit.os == "OSX"
-local isAndroid = os.getenv("ANDROID_ROOT") ~= nil
-local scriptDir = debug.getinfo(1, "S").source:sub(2):match("(.*[/\\])")
-local src = scriptDir .. "vendor" .. sep .. "libgit2"
 local libName = isWindows and "git2.dll" or (isMac and "libgit2.dylib" or "libgit2.so")
-local outLib = outDir .. sep .. libName
 
-if io.open(outLib, "rb") then return end
+local url = "https://github.com/libgit2/libgit2/archive/refs/tags/v1.8.5.tar.gz"
+local tarball = "libgit2-1.8.5.tar.gz"
 
-local function exec(cmd)
-	local ret = os.execute(cmd)
-	assert(ret == 0 or ret == true, "command failed: " .. cmd)
-end
+local content = build:fetch(url)
+build:write(tarball, content)
+build:extract(tarball, ".")
+build:move("libgit2-1.8.5", "libgit2")
 
-local build = src .. sep .. "build"
+local srcDir = build.outDir .. "/libgit2"
+local buildDir = srcDir .. "/build"
 
 ---@format disable-next
 local gitMin = "-DBUILD_TESTS=OFF -DBUILD_CLI=OFF -DUSE_SSH=OFF -DUSE_GSSAPI=OFF -DUSE_NTLMCLIENT=OFF -DREGEX_BACKEND=builtin -DUSE_HTTP_PARSER=builtin -DUSE_BUNDLED_ZLIB=ON -DCMAKE_C_FLAGS=-g0"
 
-local https, cmakeExtra
+local https
 if isWindows then
 	https = "WinHTTP"
-	cmakeExtra = ""
 elseif isMac then
 	https = "SecureTransport"
-	cmakeExtra = ""
 else
 	https = "OpenSSL"
-	cmakeExtra = ""
 end
 
-exec('cmake -S "' .. src .. '" -B "' .. build .. '" -DBUILD_SHARED_LIBS=ON -DUSE_HTTPS=' .. https .. ' ' .. gitMin .. (cmakeExtra ~= "" and (' ' .. cmakeExtra) or ""))
-exec('cmake --build "' .. build .. '" --config Release' .. (isWindows and "" or " -j$(nproc)"))
+build:sh('cmake -S "' ..
+srcDir .. '" -B "' .. buildDir .. '" -DBUILD_SHARED_LIBS=ON -DUSE_HTTPS=' .. https .. ' ' .. gitMin)
+build:sh('cmake --build "' .. buildDir .. '" --config Release' .. (isWindows and "" or " -j$(nproc)"))
 
 if isWindows then
-	exec('copy "' .. build .. '\\Release\\git2.dll" "' .. outLib .. '"')
+	build:copy("libgit2/build/Release/git2.dll", libName)
 elseif isMac then
-	exec('cp "' .. build .. '/libgit2.dylib" "' .. outLib .. '"')
-	exec('strip -x "' .. outLib .. '"')
+	build:copy("libgit2/build/libgit2.dylib", libName)
+	build:sh('strip -x "' .. build.outDir .. '/' .. libName .. '"')
 else
-	exec('cp "' .. build .. '/libgit2.so" "' .. outLib .. '"')
-	exec('strip --strip-unneeded --remove-section=.eh_frame --remove-section=.eh_frame_hdr "' .. outLib .. '"')
+	build:copy("libgit2/build/libgit2.so", libName)
+	build:sh('strip --strip-unneeded --remove-section=.eh_frame --remove-section=.eh_frame_hdr "' ..
+		build.outDir .. '/' .. libName .. '"')
 end
